16df037a2a419ce9453d2dc1a77d7e52d57de03c
José Luis Santana <YuniorGlez@MacBook-Air-de-Jose.local>
2015-06-04 01:22:14 +0100

    animate effects for fun

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
-/* Application Sripts */
+var wScroll;
+$(window).scroll(function () {
 
-$(function () {
-  'use strict';
+  wScroll = $(this).scrollTop();
+  //  console.log('wScroll =' + wScroll);
+  //  console.log('.animate = ' + $('.animate').offset().top);
+  var elements = document.getElementsByClassName("animate");
+  var i;
+  for (i = 0; i < elements.length; i++) {
+    console.log
+    if (wScroll > $(elements[i]).offset().top - $(window).height() /1.1) {
+      $(elements[i]).removeClass('none');
+      $(elements[i]).addClass('animated');
+      $(elements[i]).removeClass('animate');
+
+    }
+  }
+
 });
