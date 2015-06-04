809200ce525bb4b3eb445043a3c61726295950b9
José Luis Santana <YuniorGlez@MacBook-Air-de-Jose.local>
2015-06-04 01:47:06 +0100

    TODO fix that all elements animate when scroll

--- js/app.js
+++ js/app.js
@@ -1,18 +1,20 @@
 var wScroll;
+var elements;
+var activados = 0;
 $(window).scroll(function () {
 
   wScroll = $(this).scrollTop();
   //  console.log('wScroll =' + wScroll);
   //  console.log('.animate = ' + $('.animate').offset().top);
-  var elements = document.getElementsByClassName("animate");
+  elements = document.getElementsByClassName("animate");
   var i;
   for (i = 0; i < elements.length; i++) {
-    console.log
-    if (wScroll > $(elements[i]).offset().top - $(window).height() /1.1) {
+    console.log($(elements[i]) + ' ' +  $(elements[i]).offset().top);
+    if (wScroll > $(elements[i]).offset().top - 100) {
       $(elements[i]).removeClass('none');
       $(elements[i]).addClass('animated');
       $(elements[i]).removeClass('animate');
-
+      activados++;
     }
   }
 
