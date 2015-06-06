b910d21cf34e22d20c55e9ff131babd772efad39
José Luis Santana <YuniorGlez@MacBook-Air-de-Jose.local>
2015-06-06 13:35:02 +0100

    Bug fixed !! parallax in work ! Now, responsive !

--- js/app.js
+++ js/app.js
@@ -1,29 +1,18 @@
 
-var elements = document.getElementsByClassName("animate");
+var elements = document.getElementsByClassName("animated");
 var activados = 0;
 var actual;
 $(window).scroll(function () {
   var wScroll = $(this).scrollTop();
   var i;
-  console.log('wScroll = ' + wScroll + '  a1 = ' + ($("#a1").offset().top));
-  if (wScroll > $('#a1').offset().top/* - $(window).height*0.8*/){
-      $('#a1').removeClass('none');
-      $('#a1').addClass('animated');
-      $('#a1').removeClass('animate');
+  for (i = 0; i < elements.length; i++) {
+
+    if (wScroll > $(elements[i]).offset().top - $(window).height() * 0.8) {
+      console.log("Active " + $(elements[i]).attr("id"));
+      $(elements[i]).removeClass('none');
+      elements[i].className = elements[i].className.replace('-','');
+    }
   }
-//  for (i = 0; i < elements.length; i++) {
-//    actual = $('#' + $(elements[i]).attr("id"));
-//
-////    console.log(actual.offset().top);
-//
-//    if (wScroll > actual.offset().top) {
-//      console.log(actual.attr("id") + ' esta visible');
-//      actual.removeClass('none');
-//      actual.addClass('animated');
-//      actual.removeClass('animate');
-//      break;
-//    }
-//  }
 
 
 });
